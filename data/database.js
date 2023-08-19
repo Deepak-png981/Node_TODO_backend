e8b975a39ee651a4fdcebce55f0d46abffdd4ecc
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-08-19 14:35:04 +0530

    final commit

--- data/database.js
+++ data/database.js
@@ -4,8 +4,8 @@ export const connectDB = () => {
     mongoose
         .connect(process.env.MONGO_URI, {
             dbName: "backendAPI"
-        }).then(() => {
-            console.log("Database connected");
+        }).then((c) => {
+            console.log(`Database connected ${c.connection.host}`);
         }).catch((err) => {
             console.log(err);
         })
